32af74f072354c44af91fd04cd447b29d697ee46
Jake Hill <jake@robotire.com>
2023-09-07 12:44:03 -0400

    Working through responsive mobile views

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
-const appHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty(" --app-height", `${window.innerHeight}px`);
-};
+var countDownDate = new Date("Oct 21, 2023").getTime();
 
-window.addEventListener("resize", appHeight);
-appHeight();
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+function setOverlay() {
+  document.getElementById("overlay").style.visibility = "visible";
+}
+
+function test() {
+  document.getElementById("moreInfo-container").style.height = "150%";
+  document.getElementById("close-icon").style.display = "block";
+}
+
+function closeTest() {
+  console.log("test");
+  document.getElementById("moreInfo-container").style.height = "200%";
+}
+
+function closeOverlay() {
+  document.getElementById("overlay").style.visibility = "hidden";
+}
