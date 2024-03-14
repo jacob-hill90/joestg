e6730b8533ec1548df3d9d5d3c9c7865f86b2399
Jake <hilljake93@gmail.com>
2024-03-13 23:30:44 -0400

    baseline

---
+++ main.js
@@ -0,0 +1,31 @@
+const __days = document.querySelector("#days");
+const __hours = document.querySelector("#hours");
+const __minutes = document.querySelector("#minutes");
+const __seconds = document.querySelector("#seconds");
+const newYearEnd = "Oct 24 2024 00:00:00";
+
+function countdown() {
+  const newYearEndDate = new Date(newYearEnd);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearEndDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  __days.innerHTML = formatTime(days);
+  __hours.innerHTML = formatTime(hours);
+  __minutes.innerHTML = formatTime(minutes);
+  __seconds.innerHTML = formatTime(seconds);
+  // console.log(seconds)
+  // console.log(minutes)
+  // console.log(hours)
+}
+countdown();
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
