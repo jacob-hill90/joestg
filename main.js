40128fd7898f6338b6ac67a9990cab9ff8a81cab
Jake <hilljake93@gmail.com>
2024-08-01 19:10:12 -0400

    added link

--- main.js
+++ main.js
@@ -49,20 +49,20 @@ function showGallery() {
   document.getElementById("gallery-container").style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var coll = document.getElementsByClassName("collapsible");
-  for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var coll = document.getElementsByClassName("collapsible");
+//   for (var i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function () {
+//       this.classList.toggle("active");
+//       var content = this.nextElementSibling;
+//       if (content.style.display === "block") {
+//         content.style.display = "none";
+//       } else {
+//         content.style.display = "block";
+//       }
+//     });
+//   }
+// });
 
 // function showPreview() {
 //   document.getElementById("default-bottom-container").style.display = "flex";
